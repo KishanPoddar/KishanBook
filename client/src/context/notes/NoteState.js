6a8651470cb1360d4ce4dca18633433760e39245
KishanPoddar <kishanpoddar2002@gmail.com>
2023-02-22 20:34:56 +0530

    removed local host

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -2,14 +2,13 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
   //Get all notes
   const getNotes = async () => {
     //API call
-    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+    const response = await fetch(`/api/notes/fetchallnotes`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -23,7 +22,7 @@ const NoteState = (props) => {
   //Add a note
   const addNote = async (title, description, tag) => {
     //API call
-    const response = await fetch(`${host}/api/notes/addnote`, {
+    const response = await fetch(`/api/notes/addnote`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -40,7 +39,7 @@ const NoteState = (props) => {
   //Delete a note
   const deleteNote = async (id) => {
     //API call
-    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+    const response = await fetch(`/api/notes/deletenote/${id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -57,7 +56,7 @@ const NoteState = (props) => {
   //Edit a note
   const editNote = async (id, title, description, tag) => {
     //API call
-    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+    const response = await fetch(`/api/notes/updatenote/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -79,15 +78,6 @@ const NoteState = (props) => {
         break;
       }
     }
-    // for (let index = 0; index < newNotes.length; index++) {
-    //   const element = newNotes[index];
-    //   if (element._id === id) {
-    //     newNotes[index].title = title;
-    //     newNotes[index].description = description;
-    //     newNotes[index].tag = tag;
-    //     break;
-    //   }
-    // }
     setNotes(newNotes)
   }
 
